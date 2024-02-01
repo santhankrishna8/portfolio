c84b4ada36034f58cf18eda751aa71c0959f765b
santhan <santhankrishna18@gmail.com>
2024-02-01 15:34:46 +0530

    Add existing project files to Git

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SkillsComponent } from './skills/skills.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
+    {path:'',component:HomeComponent},
     {path:'home',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'skills',component:SkillsComponent},
